5de963d4742d7b015633a651f13e843d7c7b7096
lokeshzenbook-coder <lokeshzenbook@gmail.com>
2026-06-26 18:08:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"   # replace with your S3 bucket name
     key            = "terraform/state/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"        # replace with your DynamoDB table name
     encrypt        = true
   }
