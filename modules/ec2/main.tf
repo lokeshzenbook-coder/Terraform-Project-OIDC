cc8fb9f184442f7b12a79b68632fc978245d9d92
lokeshzenbook-coder <lokeshzenbook@gmail.com>
2026-06-26 17:55:03 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "this" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  iam_instance_profile        = var.iam_instance_profile
+  associate_public_ip_address = var.associate_public_ip
+
+  # Enforce IMDSv2
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = var.root_volume_size
+    encrypted             = true
+    delete_on_termination = true
+  }
+
+  monitoring = true
+
+  tags = {
+    Name = "${var.name_prefix}-ec2"
+  }
+
+  lifecycle {
+    ignore_changes = [ami]
+  }
+}
